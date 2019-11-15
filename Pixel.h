0d940f67fa3f338a43dabe60344080549887f3dd
Mikhail Gorokhov <mikhail.gorokhov@vicuesoft.com>
2019-11-15 19:33:43 +0700

    Fix a bug

--- Pixel.h
+++ Pixel.h
@@ -3,18 +3,18 @@
 struct Pixel
 {
 public:
-    uint8_t channels[4];
+    uint8_t m_channels[4];
 
     Pixel()
     {
         for (int i = 0; i < 4; ++i)
-            channels[i] = 255;
+            m_channels[i] = 255;
     }
 
     void swap(uint8_t *channels)
     {
         //{ "AYUV", "VUYA", "ARGB", "BGRA", "RGB24" };
         for (int i = 0; i < 4; ++i)
-            channels[i] = channels[i];
+            m_channels[i] = channels[i];
     }
 };
